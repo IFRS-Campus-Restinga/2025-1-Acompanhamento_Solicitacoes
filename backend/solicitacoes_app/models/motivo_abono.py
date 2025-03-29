dd5133bb2dafa8bc4e8a6b25ad5eb7c19864ef9b
Jampier <jampierbrunetto1@gmail.com>
2025-03-29 16:39:11 -0300

    Modificações nos models

--- backend/solicitacoes_app/models/motivo_abono.py
+++ backend/solicitacoes_app/models/motivo_abono.py
@@ -4,8 +4,19 @@ from .tipo_falta import TipoFalta
 from django.core.validators import MinLengthValidator
 
 class MotivoAbono(BaseModel):
-    descricao = models.CharField(required=True, max_length=255, validator=(MinLengthValidator(10)), blank=False, verbose_name="Descrição")
-    tipo_falta = models.CharField(required=True, max_length=2, choices=TipoFalta.choices, verbose_name="Tipo de Falta")
+    descricao = models.CharField(
+        required=True, 
+        max_length=255, 
+        validator=(MinLengthValidator(10)), 
+        blank=False, 
+        verbose_name="Descrição"
+    )
+    tipo_falta = models.CharField(
+        required=True, 
+        max_length=2, 
+        choices=TipoFalta.choices, 
+        verbose_name="Tipo de Falta"
+    )
 
     def __str__(self):
         return self.descricao
