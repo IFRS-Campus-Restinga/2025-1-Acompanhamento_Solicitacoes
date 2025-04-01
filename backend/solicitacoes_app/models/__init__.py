da46a5119e7651950e2bd4c6ff0bf8dcfac3f9eb
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-03-31 23:20:06 -0300

    Adicionando motivo de exercicios domiciliares aos inits

--- backend/solicitacoes_app/models/__init__.py
+++ backend/solicitacoes_app/models/__init__.py
@@ -3,5 +3,6 @@ from .ppc import *
 from .motivo_abono import *
 from .tipo_falta import *
 from .motivo_dispensa import *
+from .motivo_exercicios import *
 from .coordenador import *
 from .cre import *
--- backend/solicitacoes_app/views/__init__.py
+++ backend/solicitacoes_app/views/__init__.py
@@ -3,4 +3,5 @@ from .curso_view import *
 from .ppc_view import *
 from .motivo_abono_view import *
 from .motivo_dispensa_view import *
+from .motivo_exercicios_view import *
 from .coordenador_view import *
