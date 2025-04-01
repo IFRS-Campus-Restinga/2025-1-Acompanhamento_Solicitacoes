5190b673c754da1f1e03d103aafe1e2672ac85ce
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-03-31 23:36:27 -0300

    Criando Enum de Status para Solicitações

---
+++ backend/solicitacoes_app/models/status.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Status(models.TextChoices):
+    PEDIDO_CRIADO = "Pedido Criado"
+    EM_ANALISE = "Em Análise"
+    EM_EMISSAO = "Em Emissão"
+    REGISTRADO = "Registrado"
+    INDEFERIDO = "Indeferido"
+    APROVADO = "Aprovado"
+
