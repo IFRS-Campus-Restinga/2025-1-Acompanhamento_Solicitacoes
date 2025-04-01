640a9c935309209bbc435ecf0c8b505d2115cc3d
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-03-31 22:57:25 -0300

    Criação de View para motivo de exercícios domiciliares
    Adicionando view para listar descrições de todos os motivos de exercicios domiciliares.

---
+++ backend/solicitacoes_app/views/motivo_exercicios_view.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework.views import APIView
+from ..models.motivo_exercicios import MotivoExercicios
+from ..serializers.motivo_exercicios_serializer import MotivoExerciciosSerializer
+
+class ListarMotivoExercicios(APIView):
+    def get(self, request):
+        serializer = MotivoExerciciosSerializer(MotivoExercicios.objects.all(), many = True, context={"request:", request})
+        return Response(serializer.data, status=HTTP_200_OK)
+
