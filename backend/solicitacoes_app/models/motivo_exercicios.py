a552521437e73247b230998b6f0e27a6e8c2ab97
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-03-31 22:42:25 -0300

    Criação de model "MotivoExercicios"
    modelo com descrição

---
+++ backend/solicitacoes_app/models/motivo_exercicios.py
@@ -0,0 +1,10 @@
+from .base import BaseModel
+from django.db.models import CharField
+from django.core.validators import MinLengthValidator
+
+class MotivoExercicios(BaseModel):
+    descricao = CharField(max_length=200, validators=[MinLengthValidator(9)], help_text="Digite o motivo da dispensa", verbose_name="Descrição")
+
+
+    def __str__(self):
+        return self.descricao
