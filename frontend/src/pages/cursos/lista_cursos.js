5f782adae085d13f1f731905dedcd36fe583cfca
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-02 01:06:07 -0300

    Criando lista de cursos

---
+++ frontend/src/pages/cursos/lista_cursos.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Navbar from "../components/navbar";
+
+const Cursos = () => {
+  const [cursos, setCursos] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/solicitacoes/cursos/")
+      .then(res => setCursos(res.data))
+      .catch(err => console.error("Erro ao buscar cursos:", err));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <main>
+        <h2>Lista de Cursos</h2>
+        <ul>
+          {cursos.map((curso) => ( // Corrigido para "curso"
+            <li key={curso.id}>{curso.nome}</li>
+          ))}
+        </ul>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Cursos;
