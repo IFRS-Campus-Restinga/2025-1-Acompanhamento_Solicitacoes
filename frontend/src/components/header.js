969e77c55b5517bf9d63102b849a7b93436203b3
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-02 00:54:00 -0300

    Criando header base da página

---
+++ frontend/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const header = () => {
+  return (
+    <header>
+        <img src="/img/logo-ifrs-branco.png" alt="logotipo do ifrs campus restinga" class="logo"></img>
+    </header>
+  );
+};
+
+export default header;
