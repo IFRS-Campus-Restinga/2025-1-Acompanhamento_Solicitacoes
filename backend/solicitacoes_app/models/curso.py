022a1d29bd79e69563467791c5b9331021b6aa72
Santzu-26 <gabi280402@gmail.com>
2025-04-03 20:21:07 -0300

    Validações curso e ppc

--- backend/solicitacoes_app/models/curso.py
+++ backend/solicitacoes_app/models/curso.py
@@ -1,13 +1,17 @@
 from .base import BaseModel
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 class Curso(BaseModel):
     nome = models.CharField(
-        max_length=255
+        max_length=255,
+        validators=[MinLengthValidator(6)]  
     )
+    
     codigo = models.CharField(
         primary_key=True,
-        max_length=50
+        max_length=50,
+        validators=[MinLengthValidator(3)] 
     )
     
     def __str__(self):
--- backend/solicitacoes_app/models/ppc.py
+++ backend/solicitacoes_app/models/ppc.py
@@ -1,22 +1,23 @@
 from .base import BaseModel
 from django.db import models
+from django.core.validators import *
 from .curso import Curso
+
 
 class Ppc(BaseModel):
     codigo = models.CharField(
         primary_key=True,
         max_length=30,
-        null=False,
-        blank=False, 
+        validators=[MinLengthValidator(5)]  
     )
+    # Exemplo: "ADS-101/2017"
     
     curso = models.ForeignKey(
         Curso,
         on_delete=models.CASCADE,
-        related_name='ppcs',
-        null=False,
-        blank=False
+        related_name='ppcs'
     )
     
     def __str__(self):
         return self.codigo
+
