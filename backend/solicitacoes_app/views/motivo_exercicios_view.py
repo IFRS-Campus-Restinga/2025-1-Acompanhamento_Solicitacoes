dc3c32cc2f6ec6d19dbb2ca4a7dca4e995501ce0
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-06 21:34:02 -0300

    Mudando views de motivo de exercicios domiciliares para generics

--- backend/solicitacoes_app/views/motivo_exercicios_view.py
+++ backend/solicitacoes_app/views/motivo_exercicios_view.py
@@ -1,44 +1,57 @@
-from rest_framework.decorators import api_view, permission_classes
+from functools import partial
+
+from django.core.serializers import serialize
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK
-from rest_framework.views import APIView
+
 from ..models.motivo_exercicios import MotivoExercicios
 from ..serializers.motivo_exercicios_serializer import MotivoExerciciosSerializer
 
-class ListarMotivoExercicios(APIView):
-    def get(self, request):
-        serializer = MotivoExerciciosSerializer(MotivoExercicios.objects.all(), many = True, context={"request:", request})
-        return Response(serializer.data, status=HTTP_200_OK)
 
-class CadastrarMotivoExercicios(APIView):
-    def post(self, request):
-        serializer = MotivoExerciciosSerializer(data=request.data)
+class ListarMotivoExercicios(generics.ListAPIView):
+    queryset = MotivoExercicios.objects.all()
+    serializer_class = MotivoExerciciosSerializer
+
+class CadastrarMotivoExercicios(generics.CreateAPIView):
+    queryset = MotivoExercicios.objects.all()
+    serializer_class = MotivoExerciciosSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response("Motivo de exercicios domiciliares cadastrado com sucesso!", status=HTTP_201_CREATED)
+            self.perform_create(serializer)
+            return Response({'message': "Motivo de exercicios domiciliares cadastrado com sucesso!"}, status=HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-class AtualizarMotivoExercicios(APIView):
-    def put(self, request, id):
-        try:
-            motivo_exercicios = MotivoExercicios.objects.get(id=id)
-        except MotivoExercicios.DoesNotExist:
-            return Response("Motivo de exercicios domiciliares não encontrado", HTTP_404_NOT_FOUND)
-        
-        serializer = MotivoExerciciosSerializer(motivo_exercicios, data=request.data, partial=True)
+class AtualizarMotivoExercicios(generics.UpdateAPIView):
+    queryset = MotivoExercicios.objects.all()
+    serializer_class = MotivoExerciciosSerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'pk'
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer= self.get_serializer(instance, data=request.data, partial=True)
 
         if serializer.is_valid():
             serializer.save()
-            return Response("Motivo de exercicios domiciliares atualizado com sucesso!", status= HTTP_200_OK)
+            return Response({'message':"Motivo de exercicios domiciliares atualizado com sucesso!"}, status= HTTP_200_OK)
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-        
-class DeletarMotivoExercicios(APIView):
-    def delete(self, request, id):
-        try:
-            motivo_exercicios = MotivoExercicios.objects.get(id=id)
-            motivo_exercicios.delete()
-            return Response ("Motivo de exercicios domiciliares excluído com sucesso!", status=HTTP_200_OK)
-        except MotivoExercicios.DoesNotExist:
-            return Response("Motivo de exercicios domiciliares não encontrado", status=HTTP_404_NOT_FOUND)
+
+class DeletarMotivoExercicios(generics.DestroyAPIView):
+    queryset = MotivoExercicios.objects.all()
+    serializer_class = MotivoExerciciosSerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'pk'
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response ({'message': "Motivo de exercícios domiciliares excluído com sucesso!"}, status=HTTP_200_OK)
+
+
