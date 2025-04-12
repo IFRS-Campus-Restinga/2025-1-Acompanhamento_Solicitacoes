c58f0cc240e9f75f13dc876071101b19f2a8f70b
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-12 18:41:25 -0300

    Modificando para deixar a tela inicial de cruds e adicionando routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
 import axios from "axios";
-import React from "react";
-import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
-//import { Link } from "react-router-dom";
-import "./App.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Alunos from "./pages/alunos/lista_alunos";
-import Home from "./pages/home";
-//import Solicitacoes from "./pages/solicitacoes";
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import routes from "./routes/routes";
 import "./var.css";
-import ListarMotivosAbono from "./pages/motivos/abono/lista_abono";
-import CadastrarAtualizarAbono from "./pages/motivos/abono/cadastrar_atualizar_abono";
 
 class App extends React.Component {
   state = {
@@ -17,7 +12,7 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    axios.get("http://localhost:8000/solicitacoes/alunos/")
+    axios.get("http://localhost:8000/solicitacoes/")
       .then(() => this.setState({ isConnected: true }))
       .catch(err => {
         console.error("Erro na conexão:", err);
@@ -28,26 +23,8 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        {/* Barra de navegação adicionada */}
-         {/*
-        <nav>
-          <Link to="/">Início</Link>
-          <Link to="/alunos">Alunos</Link>
-          <Link to="/home">Home</Link>
-        </nav>
-         */}
-
         {this.state.isConnected ? (
-          <Routes>
-            <Route path="/" element={<Alunos />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/alunos" element={<Alunos />} />
-
-            {/* Motivos de Abono */}
-            <Route path="/motivo_abono/" element={<ListarMotivosAbono />} />
-            <Route path="/motivo_abono/cadastrar" element={<CadastrarAtualizarAbono />} />
-            <Route path="/motivo_abono/:id" element={<CadastrarAtualizarAbono />} />
-          </Routes>
+          <Routes>{routes}</Routes>
         ) : (
           <div className="error-container">
             <h2>Erro de Conexão</h2>
