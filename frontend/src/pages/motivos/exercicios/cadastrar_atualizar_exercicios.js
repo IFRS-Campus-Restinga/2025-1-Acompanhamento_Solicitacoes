91dc6497c34162b5f4cf897a0f15592ca6a41b4e
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-12 18:48:18 -0300

    Melhorando o exercicios para o padrão de abono já criado

--- frontend/src/pages/motivos/exercicios/cadastrar_atualizar_exercicios.js
+++ frontend/src/pages/motivos/exercicios/cadastrar_atualizar_exercicios.js
@@ -1,30 +1,21 @@
-import React, { useState, useEffect } from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Footer from "../../../components/footer";
 import Header from "../../../components/header";
 import Navbar from "../../../components/navbar";
-import Footer from "../../../components/footer";
-import PopupFeedback from "./popup_feedback";
 import "./motivo_exercicios.css";
+import PopupFeedback from "./popup_feedback";
 
-export default function CadastrarAtualizarAbono() {
+export default function CadastrarAtualizarExercicios() {
   const [descricao, setDescricao] = useState("");
-  const [tipoFalta, setTipoFalta] = useState("");
-  const [tipos, setTipos] = useState([]);
   const [showFeedback, setShowFeedback] = useState(false);
   const [mensagem, setMensagem] = useState("");
   const [tipoMensagem, setTipoMensagem] = useState("sucesso");
   const navigate = useNavigate();
   const { id } = useParams();
 
-/*
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/solicitacoes/motivo_exercicios/tipos/")
-      .then((res) => setTipos(res.data))
-      .catch((err) => console.error("Erro ao carregar tipos de falta:", err));
-  }, []);
-*/
+
   useEffect(() => {
     if (id) {
       axios.get(`http://localhost:8000/solicitacoes/motivo_exercicios/${id}/`)
@@ -41,7 +32,7 @@ export default function CadastrarAtualizarAbono() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const dados = {descricao};
+    const dados = { descricao};
 
     const requisicao = id
       ? axios.put(`http://localhost:8000/solicitacoes/motivo_exercicios/${id}/`, dados)
@@ -65,7 +56,7 @@ export default function CadastrarAtualizarAbono() {
       <Header />
       <Navbar />
       <main className="container form-container">
-        <h2>{id ? "Editar Motivo de Exercicios Domiciliares" : "Cadastrar Novo Motivo de Exercicios Domiciliares"}</h2>
+        <h2>{id ? "Editar Motivo de Exercicios" : "Cadastrar Novo Motivo de Exercicios"}</h2>
         <form className="form-box" onSubmit={handleSubmit}>
           <div className="form-group">
             <label>Descrição:</label>
@@ -76,24 +67,6 @@ export default function CadastrarAtualizarAbono() {
               required
             />
           </div>
-            {/*
-          <div className="form-group">
-            <label>Tipo de Falta:</label>
-            <select
-              className="input-select"
-              value={tipoFalta}
-              onChange={(e) => setTipoFalta(e.target.value)}
-              required
-            >
-              <option value="">Selecione um tipo</option>
-              {tipos.map((tipoOption) => (
-                <option key={tipoOption.value} value={tipoOption.value}>
-                  {tipoOption.label}
-                </option>
-              ))}
-            </select>
-          </div>
-           Comentário */}
 
           <button type="submit" className="submit-button">
             {id ? "Atualizar" : "Cadastrar"}
@@ -106,7 +79,7 @@ export default function CadastrarAtualizarAbono() {
           tipo={tipoMensagem}
           onClose={() => {
             setShowFeedback(false);
-            navigate("/motivo_exercicios");
+            navigate("/motivo_exercicios"); 
           }}
         />
       </main>
