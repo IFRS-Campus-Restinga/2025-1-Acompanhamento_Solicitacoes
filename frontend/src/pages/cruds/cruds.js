f39fafe4ccf066d754e1872824f9fc2d27fe63ac
Fernando Guerreiro <142168052+Nando2303@users.noreply.github.com>
2025-04-15 22:41:07 -0300

    reorganização da lista de cruds na página inicial

--- frontend/src/pages/cruds/cruds.js
+++ frontend/src/pages/cruds/cruds.js
@@ -20,32 +20,32 @@ const Cruds = () => {
       <main className="container">
         <h2>CRUDs Disponíveis</h2>
         <div className="grid-cruds">
-          <Link className="crud-link" to="/motivo_exercicios">
-            <i className="bi bi-journal-text"></i> Exercícios Domiciliares
-          </Link>
-          <Link className="crud-link" to="/motivo_abono">
-            <i className="bi bi-calendar-x-fill"></i> Abono de Falta
-          </Link>
-          <Link className="crud-link" to="/disciplinas">
-            <i className="bi bi-book"></i> Disciplina
-          </Link>
-          <Link className="crud-link" to="/turmas">
-            <i className="bi bi-people"></i> Turma
-          </Link>
-          <Link className="crud-link" to="/motivo_dispensa">
-                <i className="bi bi-person-arms-up"></i> Dispensa de Educação Física
-          </Link>
-          <Link className="crud-link" to="/cursos">
-            <i className="bi bi-mortarboard"></i> Cursos
-          </Link>
-          <Link className="crud-link" to="/ppcs">
-            <i className="bi bi-layout-text-window-reverse"></i> PPC
-          </Link>
           <Link className="crud-link" to="/usuarios">
             <i className="bi bi-person-circle"></i> Usuarios
           </Link>
           <Link className="crud-link" to="/grupos">
-            <i className="bi bi-people"></i> Grupos
+            <i className="bi bi-people-fill"></i> Grupos
+          </Link>
+          <Link className="crud-link" to="/turmas">
+            <i className="bi bi-people"></i> Turma
+          </Link>
+          <Link className="crud-link" to="/disciplinas">
+            <i className="bi bi-book"></i> Disciplina
+          </Link>
+          <Link className="crud-link" to="/ppcs">
+            <i className="bi bi-layout-text-window-reverse"></i> PPC
+          </Link>
+          <Link className="crud-link" to="/cursos">
+            <i className="bi bi-mortarboard"></i> Cursos
+          </Link>
+          <Link className="crud-link" to="/motivo_abono">
+            <i className="bi bi-calendar-x-fill"></i> Abono de Falta
+          </Link>
+          <Link className="crud-link" to="/motivo_exercicios">
+            <i className="bi bi-journal-text"></i> Exercícios Domiciliares
+          </Link>
+          <Link className="crud-link" to="/motivo_dispensa">
+            <i className="bi bi-person-arms-up"></i> Dispensa de Educação Física
           </Link>
         </div>
       </main>
