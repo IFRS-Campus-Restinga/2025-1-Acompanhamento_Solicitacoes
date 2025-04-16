155260ea860118c7707ee4c4950a6674ff3e55e8
Fernando Guerreiro <142168052+Nando2303@users.noreply.github.com>
2025-04-15 22:47:27 -0300

    reajuste bobo na mesma coisa do push anterior

--- frontend/src/pages/cruds/cruds.js
+++ frontend/src/pages/cruds/cruds.js
@@ -21,25 +21,25 @@ const Cruds = () => {
         <h2>CRUDs Disponíveis</h2>
         <div className="grid-cruds">
           <Link className="crud-link" to="/usuarios">
-            <i className="bi bi-person-circle"></i> Usuarios
+            <i className="bi bi-person-circle"></i> Usuários
           </Link>
           <Link className="crud-link" to="/grupos">
             <i className="bi bi-people-fill"></i> Grupos
           </Link>
           <Link className="crud-link" to="/turmas">
-            <i className="bi bi-people"></i> Turma
+            <i className="bi bi-people"></i> Turmas
           </Link>
           <Link className="crud-link" to="/disciplinas">
-            <i className="bi bi-book"></i> Disciplina
+            <i className="bi bi-book"></i> Disciplinas
           </Link>
           <Link className="crud-link" to="/ppcs">
-            <i className="bi bi-layout-text-window-reverse"></i> PPC
+            <i className="bi bi-layout-text-window-reverse"></i> PPCs
           </Link>
           <Link className="crud-link" to="/cursos">
             <i className="bi bi-mortarboard"></i> Cursos
           </Link>
           <Link className="crud-link" to="/motivo_abono">
-            <i className="bi bi-calendar-x-fill"></i> Abono de Falta
+            <i className="bi bi-calendar-x-fill"></i> Abono de Faltas
           </Link>
           <Link className="crud-link" to="/motivo_exercicios">
             <i className="bi bi-journal-text"></i> Exercícios Domiciliares
