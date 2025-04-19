7fc7d985d5ce1503b7e22556e43206da69313539
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-18 23:18:54 -0300

    Criando modelo solicitacao

---
+++ backend/solicitacoes_app/models/solicitacao.py
@@ -0,0 +1,26 @@
+from .base import BaseModel
+from django.db import models
+from django.core.validators import MinLengthValidator
+#from .ppc import Ppc  # Importa o modelo PPC
+from .status import Status
+
+class Solicitacao(BaseModel):
+    data_solicitacao = models.DateField(
+        help_text="Escreva aqui a data da solicitação", 
+        verbose_name="Data da Solicitação:",
+    )
+    data_emissao = models.DateField(
+        help_text="Escreva aqui a data de emissão", 
+        verbose_name="Data emissão:",
+    )
+
+    status = models.CharField( 
+        max_length=20, 
+        choices=Status.choices,
+        blank=False,
+        null=False,
+        verbose_name="Status da Solicitação"
+    )
+    
+    def __str__(self):
+        return self.status
