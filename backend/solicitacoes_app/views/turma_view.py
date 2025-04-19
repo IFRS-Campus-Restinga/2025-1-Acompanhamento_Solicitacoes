147f418fd934145a0d7053731e97a57ef133f50d
Fernando Guerreiro <142168052+Nando2303@users.noreply.github.com>
2025-04-18 23:23:20 -0300

    adicionado relacionamento entre turma e disciplina no front

--- backend/solicitacoes_app/views/turma_view.py
+++ backend/solicitacoes_app/views/turma_view.py
@@ -3,21 +3,19 @@ from rest_framework.permissions import AllowAny
 from ..models import Turma
 from ..serializers.turma_serializer import TurmaSerializer
 
-
 class TurmaListCreateView(generics.ListCreateAPIView):
     """
     Endpoint para listar e criar turmas.
     """
-    queryset = Turma.objects.all()
+    queryset = Turma.objects.all().prefetch_related('disciplinas')  # Ajuste para carregar as disciplinas corretamente
     serializer_class = TurmaSerializer
     permission_classes = [AllowAny]
-
 
 class TurmaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     """
     Endpoint para recuperar, atualizar e deletar uma turma específica.
     """
-    queryset = Turma.objects.all()
+    queryset = Turma.objects.all().prefetch_related('disciplinas')  # Também aqui para garantir que as disciplinas sejam carregadas
     serializer_class = TurmaSerializer
     permission_classes = [AllowAny]
-    lookup_field = 'id'  # Se você usar o campo 'id' para recuperar a turma faça nesse molde
+    lookup_field = 'id'
