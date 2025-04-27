e2541936d2930821fcdf752c3a99dee4fba9f3a9
Fernando Guerreiro <142168052+Nando2303@users.noreply.github.com>
2025-04-27 14:11:10 -0300

    ajustado errinho nas estaticas

--- backend/solicitacoes_app/views/estaticas.py
+++ backend/solicitacoes_app/views/estaticas.py
@@ -41,6 +41,6 @@ def api_root(request, format=None):
         
         'mandato': reverse('solicitacoes_app:mandato-list', request=request, format=format),
 
-        'form_tranc_disciplina': reverse('solicitacoes_app:form-tranc-disciplina-list-create', request=request, format=format),
+        'form_tranc_disciplina': reverse('solicitacoes_app:listar_cadastrar_form_trancamento_disciplina', request=request, format=format),
         
         })
