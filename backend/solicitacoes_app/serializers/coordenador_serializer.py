5656ff5e352a07504b657e7472ed0156950337e3
Caroline Venturi <carolventuri@gmail.com>
2025-04-28 15:45:28 -0300

    alterações nos serializers do coordenador e do mandato

--- backend/solicitacoes_app/serializers/coordenador_serializer.py
+++ backend/solicitacoes_app/serializers/coordenador_serializer.py
@@ -1,78 +1,27 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from ..models import Coordenador, Mandato, Usuario, Curso
+from ..models import Coordenador
 from .mandato_serializer import MandatoSerializer
-from django.db import transaction
 
 
 class CoordenadorSerializer(serializers.ModelSerializer):
-    mandato = MandatoSerializer(source='mandatos_coordenador.first', many=False)
+    mandatos_coordenador = serializers.SerializerMethodField()
 
     class Meta:
         model = Coordenador
-        fields = ['usuario', 'siape', 'mandato']
+        fields = ['usuario', 'siape', 'mandatos_coordenador']
         
-
-    @transaction.atomic
-    def create(self, validated_data):
-        usuario_data = validated_data.pop('usuario')
-        mandato_data = validated_data.pop('mandato')
-
-        # Se usuario_data for int (id), busca o usuário
-        if isinstance(usuario_data, int):
-            usuario = get_object_or_404(Usuario, id=usuario_data)
-        elif isinstance(usuario_data, dict):
-            # Se for um dicionário, cria um novo usuário
-            usuario = Usuario.objects.create(**usuario_data)
-        else:
-            raise ValueError('Dados inválidos para o usuário.')
-
-        # Cria o Coordenador
-        coordenador = Coordenador.objects.create(
-            usuario=usuario,
-            siape=validated_data['siape']
-        )
-
-        # Cria o Mandato
-        Mandato.objects.create(
-            coordenador=coordenador,
-            curso=get_object_or_404(Curso, id=mandato_data['curso']),
-            inicio_mandato=mandato_data['inicio_mandato'],
-            fim_mandato=mandato_data.get('fim_mandato')
-        )
-
-        return coordenador
-
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        usuario_data = validated_data.pop('usuario', None)
-        mandato_data = validated_data.pop('mandato', None)
-        siape = validated_data.get('siape')
-
-        # Atualiza o Usuario
-        if usuario_data:
-            for attr, value in usuario_data.items():
-                setattr(instance.usuario, attr, value)
-            instance.usuario.save()
-
-        # Atualiza o Coordenador
-        if siape:
-            instance.siape = siape
-            instance.save()
-
-        # Atualiza ou cria Mandato
-        if mandato_data:
-            mandato = instance.mandatos_coordenador.first()
-            if mandato:
-                for attr, value in mandato_data.items():
-                    setattr(mandato, attr, value)
-                mandato.save()
-            else:
-                Mandato.objects.create(
-                    coordenador=instance,
-                    curso=mandato_data['curso'],
-                    inicio_mandato=mandato_data['inicio_mandato'],
-                    fim_mandato=mandato_data.get('fim_mandato')
-                )
-
-        return instance
+    def get_mandatos_coordenador(self, obj):
+        return [
+            {
+                'curso': mandato.curso.codigo,
+                'inicio_mandato': mandato.inicio_mandato.strftime("%d-%m-%Y") if mandato.inicio_mandato else None,
+                'fim_mandato': mandato.fim_mandato.strftime("%d-%m-%Y") if mandato.fim_mandato else None
+            }
+            for mandato in obj.mandatos_coordenador.all()
+        ]
+        
+    def validate(self, data):
+        # Executa as validações do model
+        instance = self.instance or self.Meta.model(**data)
+        instance.full_clean() 
+        return data
--- backend/solicitacoes_app/serializers/mandato_serializer.py
+++ backend/solicitacoes_app/serializers/mandato_serializer.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
-from ..models import Mandato, Curso
+from ..models import Mandato, Curso, Coordenador
 
 class MandatoSerializer(serializers.ModelSerializer):
     curso = serializers.PrimaryKeyRelatedField(queryset=Curso.objects.all())
-    coordenador = serializers.PrimaryKeyRelatedField(read_only=True)
+    coordenador = serializers.PrimaryKeyRelatedField(queryset=Coordenador.objects.all())
     inicio_mandato = serializers.DateField(format="%d-%m-%Y", input_formats=["%Y-%m-%d", "%d-%m-%Y"])
     fim_mandato = serializers.DateField(format="%d-%m-%Y", input_formats=["%Y-%m-%d", "%d-%m-%Y"])
     
     class Meta:
         model = Mandato
         fields = ['coordenador', 'curso', 'inicio_mandato', 'fim_mandato']
+    
+    def validate(self, data):
+        # Executa as validações do model
+        instance = self.instance or self.Meta.model(**data)
+        instance.full_clean() 
+        return data
