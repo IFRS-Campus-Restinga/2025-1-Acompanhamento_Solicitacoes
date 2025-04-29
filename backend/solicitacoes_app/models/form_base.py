4c2e26e66b2205fafa5a08a8a5cf7b26ee671f1f
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-28 22:59:06 -0300

    Colocando descricao como opcional para nao dar erro nos demais formularios

--- backend/solicitacoes_app/models/form_base.py
+++ backend/solicitacoes_app/models/form_base.py
@@ -3,11 +3,10 @@ from django.db import models
 # from django.core.validators import MinLengthValidator
 
 class FormularioBase(BaseModel):
-    descricao = models.TextField() #Campo que explica o objetivo do formulário em questão. Vai ser apresentado no frontend na tela de abertura do formulário.
+    descricao = models.TextField(blank=True, null=True) #Campo que explica o objetivo do formulário em questão. Vai ser apresentado no frontend na tela de abertura do formulário.
     motivo_solicitacao = models.CharField(
         max_length=255
     )
-
 
     class Meta:
         abstract = True
