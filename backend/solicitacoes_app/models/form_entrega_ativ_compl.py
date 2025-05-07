e57efc32b27dfd99675670b97c9ba6398a10a841
Santzu-26 <gabi280402@gmail.com>
2025-05-07 09:50:07 -0300

    Correção erro atividades Complementares

--- backend/solicitacoes_app/models/form_entrega_ativ_compl.py
+++ backend/solicitacoes_app/models/form_entrega_ativ_compl.py
@@ -5,26 +5,7 @@ from .curso import Curso
 from .disciplina import Disciplina
 
 class FormEntregaAtivCompl(FormularioBase):
-    aluno = models.ForeignKey(Aluno,
-                              on_delete=models.RESTRICT,
-                              verbose_name="Aluno",
-                              )
+    nome = "Formulário de Atividades Complementares"
     
-    email = models.EmailField(verbose_name="Email",
-                              help_text="Email será autopreenchido")
-    
-    nome = models.CharField(verbose_name="Nome do aluno",
-                            help_text="Nome será autopreenchido")
-    
-    matricula = models.CharField(max_length=20,
-                                 verbose_name="Matrícula",
-                                 help_text="Matrícula será autopreenchido")
-    
-    curso = models.ForeignKey(Curso,
-                              on_delete=models.RESTRICT,
-                              verbose_name="Curso",
-                              help_text="Selecione seu curso")
-    
-    disciplinas = models.ManyToManyField(Disciplina,
-                                         verbose_name="Disciplinas",
-                                         help_text="Selecione suas disciplinas")
+    class Meta:
+        verbose_name = "Formulário de Trancamento de Matrícula"
