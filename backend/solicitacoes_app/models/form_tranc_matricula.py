1288cb12300075c6b4ba6772fed4e4f341952564
Santzu-26 <gabi280402@gmail.com>
2025-05-06 22:44:15 -0300

    Update form_tranc_matricula.py

--- backend/solicitacoes_app/models/form_tranc_matricula.py
+++ backend/solicitacoes_app/models/form_tranc_matricula.py
@@ -3,6 +3,8 @@ from .form_base import FormularioBase   # sua classe abstrata
 
 class FormularioTrancamentoMatricula(FormularioBase):
     nome_formulario = "Formulário de Trancamento de Matrícula"
+
+    
     class Meta:
         verbose_name = "Formulário de Trancamento de Matrícula"
 
