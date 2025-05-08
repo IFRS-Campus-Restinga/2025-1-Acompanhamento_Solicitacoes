156996c108b56e6ccc00eafed9849cbb99859b1d
Santzu-26 <gabi280402@gmail.com>
2025-05-08 15:54:36 -0300

    Campos de solicitacao nao podem ser altrados

--- backend/solicitacoes_app/models/solicitacao.py
+++ backend/solicitacoes_app/models/solicitacao.py
@@ -7,26 +7,28 @@ from django.contrib.contenttypes.models import ContentType
 from .aluno import Aluno
 from django.core.exceptions import ValidationError
 
+
 class Solicitacao(BaseModel):
     aluno = models.ForeignKey(
         Aluno,
         related_name='aluno',
-        on_delete=models.DO_NOTHING   
+        on_delete=models.DO_NOTHING
     )
-    
+
     content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
         verbose_name="Tipo de Formulário",
         help_text="O tipo específico de formulário associado a esta solicitação.",
         limit_choices_to=models.Q(app_label='solicitacoes_app', model='formexerciciodomiciliar') |
-                         models.Q(app_label='solicitacoes_app', model='formdispensaedfisica') |
-                         models.Q(app_label='solicitacoes_app', model='formtrancdisciplina') |
-                         models.Q(app_label='solicitacoes_app', model='formabonofalta') |
-                         models.Q(app_label='solicitacoes_app', model='formdesistenciavaga') |
-                         models.Q(app_label='solicitacoes_app', model='formulariotrancamentomatricula')
+        models.Q(app_label='solicitacoes_app', model='formdispensaedfisica') |
+        models.Q(app_label='solicitacoes_app', model='formtrancdisciplina') |
+        models.Q(app_label='solicitacoes_app', model='formabonofalta') |
+        models.Q(app_label='solicitacoes_app', model='formdesistenciavaga') |
+        models.Q(app_label='solicitacoes_app',
+                 model='formulariotrancamentomatricula')
     )
-    
+
     object_id = models.PositiveIntegerField(
         verbose_name="ID do Formulário Vinculado",
         help_text="O ID da instância específica do formulário."
@@ -34,7 +36,7 @@ class Solicitacao(BaseModel):
     formulario_associado = GenericForeignKey('content_type', 'object_id')
 
     data_solicitacao = models.DateField(
-        help_text="Escreva aqui a data da solicitação", 
+        help_text="Escreva aqui a data da solicitação",
         verbose_name="Data da Solicitação:",
     )
 
@@ -43,20 +45,27 @@ class Solicitacao(BaseModel):
         null=True
     )
 
-    status = models.CharField( 
-        max_length=20, 
+    status = models.CharField(
+        max_length=20,
         choices=Status.choices,
         blank=False,
         null=False,
         verbose_name="Status da Solicitação"
     )
 
-    def clean(self):
-        ModelClass = self.content_type.model_class()
-        if not ModelClass.objects.filter(pk=self.object_id).exists():
-            raise ValidationError("O ID do formulário associado não existe para este tipo.")
-        
-    
+    def save(self, *args, **kwargs):
+        if self.pk:
+            original = Solicitacao.objects.get(pk=self.pk)
+            campos_restritos = ['aluno', 'content_type',
+                                'object_id', 'data_solicitacao']
+
+            for campo in campos_restritos:
+                if getattr(self, campo) != getattr(original, campo):
+                    raise ValidationError(
+                        f"O campo '{campo}' não pode ser alterado após a criação.")
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
         nome_aluno = self.aluno.usuario.nome if self.aluno and self.aluno.usuario else "Sem Aluno"
         if self.formulario_associado:
