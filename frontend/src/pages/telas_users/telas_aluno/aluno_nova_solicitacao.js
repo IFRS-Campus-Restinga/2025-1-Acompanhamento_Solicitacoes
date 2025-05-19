a1ccceb890b9438babc8d23a9e1d5dc25c46415e
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-05-19 16:38:22 -0300

    Criando nova-solicitacao e excluindo da pasta genérica solicitacoes

--- frontend/src/pages/telas_users/telas_aluno/aluno_nova_solicitacao.js
+++ frontend/src/pages/telas_users/telas_aluno/aluno_nova_solicitacao.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Footer from "../../components/base/footer";
-import Header from "../../components/base/header";
-import "./../configuracoes/cruds";
+import Footer from "../../../components/base/footer";
+import HeaderAluno from "../../../components/base/headers/header_aluno";
+import "./../../configuracoes/cruds";
 
-const NovaSolicitacao = () => {
+const AlunoNovaSolicitacao = () => {
   const [solicitacoes, setSolicitacoes] = useState([]);
 
   useEffect(() => {
@@ -15,44 +15,11 @@ const NovaSolicitacao = () => {
       .catch((err) => console.error("Erro ao buscar solicitações:", err));
   }, []);
 
-  /* COLOCAR FUTURAMENTE NO CODIGO
-  const NovaSolicitacao = () => {
-    const [solicitacoes, setSolicitacoes] = useState([]);
-    const [usuario, setUsuario] = useState(null);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      // Aqui você vai buscar o usuário logado
-      const userInfo = JSON.parse(localStorage.getItem('usuario')); // Ajustar conforme onde está salvo
-      if (userInfo) {
-        setUsuario(userInfo);
-      } else {
-        navigate("/"); // Se não tiver usuário logado, manda pra home (ou login)
-      }
-    }, [navigate]);
-  
-    useEffect(() => {
-      if (usuario && usuario.grupo !== "Aluno") { // Ajuste o campo correto ("grupo", "role", "perfil"...)
-        alert("Acesso restrito: apenas alunos podem solicitar.");
-        navigate("/"); // Ou para onde você quiser
-      }
-    }, [usuario, navigate]);
-  
-    useEffect(() => {
-      axios.get("http://127.0.0.1:8000/solicitacoes/")
-        .then(res => setSolicitacoes(res.data))
-        .catch(err => console.error("Erro ao buscar solicitações:", err));
-    }, []);
-  
-    if (!usuario || usuario.grupo !== "Aluno") {
-      return null; // Enquanto não validar o usuário, não renderiza nada
-    }
-*/
   return (
     <div>
-      <Header />
+      <HeaderAluno />
       <main className="container">
-        <h2>Solicitações Disponíveis</h2>
+          <h2>Solicitações Disponíveis</h2>
         <div className="grid-cruds">
           <Link className="crud-link" to="/trancamento_matricula">
             <i className="bi bi-box-arrow-right"></i> Solicitação de Trancamento
@@ -88,4 +55,4 @@ const NovaSolicitacao = () => {
   );
 };
 
-export default NovaSolicitacao;
+export default AlunoNovaSolicitacao;
