51484f896509e2bd7f3203ab603f819a2d411016
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-05-21 11:51:29 -0300

    Colocando novo atributo tipo_periodo em CURSO

--- backend/solicitacoes_app/models/curso.py
+++ backend/solicitacoes_app/models/curso.py
@@ -1,8 +1,13 @@
 from .base import BaseModel
 from django.db import models
 from django.core.validators import MinLengthValidator
+from .periodo_disciplina import PeriodoDisciplina
 
 class Curso(BaseModel):
+    class TipoPeriodo(models.TextChoices):
+        ANUAL = "Anual", "Anual"
+        SEMESTRAL = "Semestral", "Semestral"
+        
     nome = models.CharField(
         max_length=255,
         validators=[MinLengthValidator(6)]  
@@ -12,6 +17,12 @@ class Curso(BaseModel):
         primary_key=True,
         max_length=50,
         validators=[MinLengthValidator(3)] 
+    )
+    
+    tipo_periodo = models.CharField(
+        max_length=10,
+        choices=TipoPeriodo.choices,
+        default=TipoPeriodo.SEMESTRAL,
     )
     
     def __str__(self):
