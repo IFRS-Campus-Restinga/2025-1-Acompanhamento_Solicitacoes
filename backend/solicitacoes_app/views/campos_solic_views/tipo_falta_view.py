ab06dae384b1bdd313f65f362ad8c2aefecd018c
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-06-25 23:39:16 -0300

    Trocando de pasta

--- backend/solicitacoes_app/views/campos_solic_views/tipo_falta_view.py
+++ backend/solicitacoes_app/views/campos_solic_views/tipo_falta_view.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from solicitacoes_app.models import TipoFalta
+from solicitacoes_app.models.campos_solic_models.tipo_falta import TipoFalta
 
 class TipoFaltaView(APIView):
     permission_classes = [AllowAny]
